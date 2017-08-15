69254a89710e11bd7eeaf0dea16837898653aa29
gopikoti <30935959+gopikoti@users.noreply.github.com>
2017-08-15 12:36:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical-one',function (req, res){
-    res.send('Artical one');
+res.sendFile(path.join(__dirname, 'ui', 'artical.html'));
 });
 app.get('/artical-two',function (req, res){
     res.send('Artical two');
